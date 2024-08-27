b05db34d7b7cb193e12e09f99469cdcca4c5a13f
aidanjjoyce <88487664+aidanjjoyce@users.noreply.github.com>
2024-08-27 10:11:30 +0100

    Update CriticalPower.py
    Updated power curve to use max-power smoothing

--- CriticalPower.py
+++ CriticalPower.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def maximum_power(critical_power, anaerobic_work_capacity, time):
-    return critical_power + anaerobic_work_capacity / time
+def maximum_power(critical_power, anaerobic_work_capacity, max_power, time):
+    return (critical_power + (max_power - critical_power) *
+            np.tanh(anaerobic_work_capacity / (time * (max_power - critical_power))))
 
-def plot_power_curve(critical_power, anaerobic_work_capacity):
-    times = np.array([t for t in range(5, 60*60, 5)])
-    max_powers = maximum_power(critical_power, anaerobic_work_capacity, times)
+def plot_power_curve(critical_power, anaerobic_work_capacity, max_power):
+    times = np.array([t for t in range(5, 60*60, 1)])
+    max_powers = maximum_power(critical_power, anaerobic_work_capacity, max_power, times)
 
-    plt.plot(times, max_powers)
+    plt.plot(times/60, max_powers)
+    plt.ylim(bottom=0, top=1.05 * max_power)
     plt.show()
     return max_powers
 
@@ -16,5 +18,6 @@ if __name__ == "__main__":
 
     cp = 250
     w = 40000
+    mp = 800
 
-    print(plot_power_curve(cp, w))
+    print(plot_power_curve(cp, w, mp))
